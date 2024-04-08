55d96f5cc170a31800d750edefc15209f2960e79
kashtan dariya <kashtandariya@gmail.com>
2024-04-08 19:39:10 +0200

    add: primo approccio al javascript

---
+++ app.js
@@ -0,0 +1,18 @@
+// Quando l’utente fa click sul bottone submit del form, il sito deve calcolare l’ammontare del preventivo per le ore di lavoro richieste.
+//quindi aggiungi addEventListener però non "click" perché l'evento è un submit di un form!!
+
+// Il prezzo finale è dato dal numero di ore * prezzo orario.
+// sviluppo backend il prezzo orario è di 20.50€/l’ora
+// sviluppo frontend il prezzo orario è di 15.30€/l’ora
+// analisi progettuale il prezzo orario è di 33.60€/l’ora
+
+// codice promozionale OPZIONALE tra i seguenti (crea un array)
+// YHDNU32, JANJC63, PWKCN25, SJDPO96, POCIE24.
+
+// Se l’utente inserisce un codice promozionale valido, ha diritto ad uno sconto del 25% sul prezzo finale. Se il codice inserito non è valido, il sito deve informare l’utente che il codice non è valido e il prezzo finale viene calcolato senza applicare sconti.
+// quindi aggiungi condizioni if else per i codici (con il metodo includes per l'array)
+// e poi un alert se il codice non è valido
+
+// Il risultato del calcolo del prezzo finale deve essere visualizzato in “forma umana” (con 2 decimali e il simbolo dell’euro)
+// quindi formatta di conseguenza
+
